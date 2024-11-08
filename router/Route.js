c0d2da6e2f1077fc1cb5e742adad7f38a672e907
Arnaldo tomo <arnaldotomo@gmail.com>
2024-11-08 07:54:05 +0200

    Update Route.js

--- router/Route.js
+++ router/Route.js
@@ -6,37 +6,38 @@ import ExtratoScreen from '../screens/ExtratoScreen'
 import Auth_login from '../screens/auth'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { ActivityIndicator, View } from 'react-native'
+import Profile from "../screens/Profile";
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
-export default function Route () {
-  const [initialRouteName, setInitialRouteName] = useState(null)
+export default function Route() {
+  const [initialRouteName, setInitialRouteName] = useState(null);
 
   useEffect(() => {
     const checkUserLoggedIn = async () => {
       try {
-        const userId = await AsyncStorage.getItem('userId')
+        const userId = await AsyncStorage.getItem("userId");
         if (userId) {
-          setInitialRouteName('Home')
+          setInitialRouteName("Home");
         } else {
-          setInitialRouteName('Auth_login')
+          setInitialRouteName("Auth_login");
         }
       } catch (error) {
-        console.error('Erro ao verificar o login:', error)
-        setInitialRouteName('Auth_login')
+        console.error("Erro ao verificar o login:", error);
+        setInitialRouteName("Auth_login");
       }
-    }
+    };
 
-    checkUserLoggedIn()
-  }, [])
+    checkUserLoggedIn();
+  }, []);
 
   if (!initialRouteName) {
     // Enquanto verifica o estado de login, exiba um indicador de carregamento
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator size='large' color='#0000ff' />
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="#0000ff" />
       </View>
-    )
+    );
   }
 
   return (
@@ -44,20 +45,25 @@ export default function Route () {
       <Stack.Navigator initialRouteName={initialRouteName}>
         <Stack.Screen
           options={{ headerShown: false }}
-          name='Auth_login'
+          name="Auth_login"
           component={Auth_login}
         />
         <Stack.Screen
           options={{ headerShown: false }}
-          name='Home'
+          name="Home"
           component={HomeScreen}
         />
         <Stack.Screen
           options={{ headerShown: false }}
-          name='ExtratoScreen'
+          name="ExtratoScreen"
           component={ExtratoScreen}
+        />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="ProfileScreen"
+          component={Profile}
         />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
